f5aa175fdd3445c0b6e4a4286c70fb753610033f
taroj khan <tarujkhan@yahoo.com>
2020-01-26 20:54:40 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,19 @@
 def square_array(array)
+  new_array = []
+  array.each do |element|
+   #element ** 2 
+  new_array.push(element ** 2) 
+end
+new_array
   # your code here
 end
+
+#def square_array(array)
+ # numbers = [1,2,3]
+  #array.each do |element|
+#def square!
+ # square.numbers "{|num| number ** 2}"
+  
+#end
+  # your code here
+#end
