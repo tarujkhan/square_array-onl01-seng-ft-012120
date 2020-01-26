5f7eedf61965be856455562ebfd596cdf3578d05
taroj khan <tarujkhan@yahoo.com>
2020-01-26 20:32:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,8 +6,8 @@ def square_array(array)
 end
 
 #def square_array(array)
-  numbers = [1,2,3]
-  array.each do |element|
+ # numbers = [1,2,3]
+  #array.each do |element|
 #def square!
  # square.numbers "{|num| number ** 2}"
   
