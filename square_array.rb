071bc2dfa4aaf33a72effd8eb931148b08db1a34
taroj khan <tarujkhan@yahoo.com>
2020-01-26 20:31:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,13 @@
 def square_array(array)
   numbers = [1,2,3]
   array.each do |element|
+
+  # your code here
+end
+
+def square_array(array)
+  numbers = [1,2,3]
+  array.each do |element|
 def square!
   square.numbers "{|num| number ** 2}"
   
